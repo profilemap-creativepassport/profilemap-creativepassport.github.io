c6d5a9a5c135f42f266d879acdfab12328672927
profilemap-creativepassport <60190927+profilemap-creativepassport@users.noreply.github.com>
2020-01-22 17:00:24 +0000

    Add files via upload

---
+++ precache-manifest.3d1e3e53494cce5323ab904f9e97e860.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94154658c4340513ed9731cb0304e901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "535943322ba9ecb5ac4c",
+    "url": "/static/css/main.91345812.chunk.css"
+  },
+  {
+    "revision": "520d89ac538a6bd1b68b",
+    "url": "/static/js/2.c63276b0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c63276b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "535943322ba9ecb5ac4c",
+    "url": "/static/js/main.a729cede.chunk.js"
+  },
+  {
+    "revision": "23ab70846ff2ed365202",
+    "url": "/static/js/runtime-main.19e5cc06.js"
+  },
+  {
+    "revision": "d26b93536f978befea0959f1245a6aea",
+    "url": "/static/media/cp-logo-wht.d26b9353.png"
+  },
+  {
+    "revision": "4773325a38cc5b0a85c80cf5c5b4ecc1",
+    "url": "/static/media/cp-text.4773325a.png"
+  }
+]);
